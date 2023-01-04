ed7d1e7ad9ce1ef6849f247e56bf6a0bb3dd4b2a
Ylfa Liang <111717563+yingfangliang@users.noreply.github.com>
2023-01-04 15:45:00 -0500

    Update easy.sql

--- easy.sql
+++ easy.sql
@@ -24,3 +24,24 @@ WHERE count > 1
 SELECT name AS Customers
 FROM Customers
 WHERE id NOT IN (SELECT DISTINCT customerId FROM Orders)
+
+# 196. Delete Duplicate Emails
+DELETE FROM Person
+WHERE id NOT IN 
+(SELECT mid 
+FROM
+(SELECT MIN(id) AS mid
+FROM Person
+GROUP BY email) AS tmp)
+
+# 197. Rising Temperature
+SELECT today.id AS Id
+FROM Weather today
+LEFT JOIN Weather yesterday
+ON DATEDIFF(today.recordDate,yesterday.recordDate) = 1
+WHERE today.temperature > yesterday.temperature
+
+# 511. Game Play Analysis I
+SELECT player_id, MIN(event_date) AS first_login
+FROM Activity
+GROUP BY player_id
