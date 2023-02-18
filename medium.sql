26097ee9f82d26a8bc267aacada10e8eb0a79e91
Ylfa Liang <111717563+yingfangliang@users.noreply.github.com>
2023-02-17 22:24:00 -0500

    Create medium.sql

---
+++ medium.sql
@@ -0,0 +1,15 @@
+# 176. Second Highest Salary
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary != (SELECT MAX(salary) FROM Employee)
+
+# 177. Nth Highest Salary
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N = N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT DISTINCT(salary) FROM Employee 
+      ORDER BY salary DESC LIMIT 1 OFFSET N
+  );
+END
