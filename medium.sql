67811f1976984aef43217b5e1f37094a33d2c7c2
Ylfa Liang <111717563+yingfangliang@users.noreply.github.com>
2023-02-21 21:10:13 -0500

    Update medium.sql

--- medium.sql
+++ medium.sql
@@ -13,3 +13,18 @@ SET N = N-1;
       ORDER BY salary DESC LIMIT 1 OFFSET N
   );
 END
+
+# 178. Rank Scores
+SELECT S.score, tmp.rank
+FROM Scores S, (SELECT DISTINCT(score), ROW_NUMBER() OVER (ORDER BY score DESC) AS 'rank'
+FROM Scores
+GROUP BY score
+ORDER BY score) AS tmp
+WHERE S.score = tmp.score
+ORDER BY S.score DESC
+
+# 180. Consecutive Numbers
+SELECT DISTINCT L1.num AS ConsecutiveNums
+FROM Logs L1, Logs L2, Logs L3 
+WHERE L1.id + 1 = L2.id AND L2.id + 1 = L3.id 
+AND L1.num = L2.num AND L2.num = L3.num
