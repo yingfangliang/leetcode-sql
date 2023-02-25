6e4e0a045666498776f9736bb70e7ce573d560c2
Ylfa Liang <111717563+yingfangliang@users.noreply.github.com>
2023-02-25 12:33:00 -0500

    Update medium.sql

--- SQL/medium.sql
+++ SQL/medium.sql
@@ -37,3 +37,11 @@ WHERE E.departmentId = D.id AND
     (SELECT departmentId, MAX(salary)
      FROM Employee
      GROUP BY departmentId)
+     
+# 608. Tree Node
+SELECT id, CASE 
+    WHEN p_id IS null THEN "Root"
+    WHEN p_id IN (SELECT id FROM tree) AND id IN (SELECT p_id FROM tree) THEN "Inner"
+    ELSE 'Leaf'
+END AS "type"
+FROM Tree     
