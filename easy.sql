849c557e0f1074977a32fc18d204e72486c911b9
Ylfa Liang <111717563+yingfangliang@users.noreply.github.com>
2023-03-03 16:02:50 -0500

    Added practice

--- easy.sql
+++ easy.sql
@@ -89,3 +89,9 @@ SELECT * FROM Cinema
 WHERE description NOT LIKE "%boring%" AND
 mod(id,2) = 1
 ORDER BY rating DESC
+
+# 627. Swap Salary
+UPDATE Salary 
+SET sex = CASE 
+            WHEN sex = 'f' THEN 'm' ELSE 'f' 
+            END 
--- medium.sql
+++ medium.sql
@@ -44,4 +44,4 @@ SELECT id, CASE
     WHEN p_id IN (SELECT id FROM tree) AND id IN (SELECT p_id FROM tree) THEN "Inner"
     ELSE 'Leaf'
 END AS "type"
-FROM Tree     
+FROM Tree    
