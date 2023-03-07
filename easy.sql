cb692b3d8f0ed8a4fac1270c69d2b6cc66e12137
Ylfa Liang <111717563+yingfangliang@users.noreply.github.com>
2023-03-07 14:02:57 -0500

    Update easy.sql

--- easy.sql
+++ easy.sql
@@ -95,3 +95,9 @@ UPDATE Salary
 SET sex = CASE 
             WHEN sex = 'f' THEN 'm' ELSE 'f' 
             END 
+            
+# 1050. Actors and Directors Who Cooperated At Least Three Times
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(*) >= 3
