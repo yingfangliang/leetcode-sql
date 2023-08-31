0368e9e85b3d69c771abeb0ae27739e52f4706b9
Ylfa Liang <111717563+yingfangliang@users.noreply.github.com>
2023-08-31 19:39:28 -0400

    added practice

--- hard.sql
+++ hard.sql
@@ -1,4 +1,4 @@
--- 185. Department Top Three Salaries
+-- 185. Department Top Three Salaries (DENSE_RANK())
 SELECT 
     d.name AS "Department",
     e.name AS "Employee",
@@ -14,3 +14,17 @@ FROM (
 LEFT JOIN Department AS d
 ON e.departmentId = d.id
 WHERE e.ranking <= 3
+
+-- 262. Trips and Users
+SELECT 
+    t.request_at AS "Day", 
+    ROUND(AVG(IF(t.status = "completed", 0, 1)), 2) AS "Cancellation Rate"
+FROM Trips t, Users c, Users d
+WHERE
+    t.client_id = c.users_id AND
+    t.driver_id = d.users_id AND
+    c.banned = "NO" AND
+    d.banned = "NO" AND
+    t.request_at >= "2013-10-01" AND
+    t.request_at <= "2013-10-03"
+GROUP BY t.request_at
